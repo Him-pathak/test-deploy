4f06153d09a4318a3eaf199268a9afa40b52b2be
Him-pathak <pathakhimanshukr@gmail.com>
2023-12-11 21:03:43 +0530

    basic env checking

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+const fname = process.env.REACT_APP_FIRST_NAME;
+const lname = process.env.REACT_APP_LAST_NAME;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello {fname} {lname}
         </p>
         <a
           className="App-link"
